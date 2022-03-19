25a5a2deaeedff8121704eafca96f24edad6a700
AnActualEmerald <kevinliger13@gmail.com>
2022-03-19 16:48:06 -0400

    Added 404 catcher to support vue routing

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate rocket;
 
 use std::{path::PathBuf, str::FromStr};
 
-use rocket::routes;
+use rocket::{catch, catchers, response::content::Html, routes};
 use rocket_contrib::serve::StaticFiles;
 
 const UNKNOWN_PAGE: &str = "404";
@@ -12,5 +12,12 @@ const UNKNOWN_PAGE: &str = "404";
 fn main() {
     rocket::ignite()
         .mount("/", StaticFiles::from("./public"))
+        .register(catchers![catcher])
         .launch();
+}
+
+#[catch(404)]
+fn catcher() -> Html<String> {
+    let path = PathBuf::from_str("./public/index.html").unwrap();
+    Html(std::fs::read_to_string(path).unwrap())
 }
